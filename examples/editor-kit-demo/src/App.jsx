e78a8926d0d358771da716b46e29ab22b1b9f67f
ray0324 <ray0324@foxmail.com>
2025-03-09 22:43:08 +0800

    chore: 初始化项目

---
+++ examples/editor-kit-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import RichTextEditor from 'editor-kit';
+
+const App = () => {
+  return (
+    <div className="container">
+      <RichTextEditor />
+    </div>
+  );
+};
+
+export default App;
---
+++ packages/editor-kit/src/index.tsx
@@ -0,0 +1,7 @@
+import RichTextEditor from './editor/richtext-editor';
+import { Button } from './Button';
+
+console.log('RichTextEditor111', RichTextEditor);
+
+export default RichTextEditor;
+export {  Button };
