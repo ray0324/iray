5e532acae68550d01a55cc2f292189ef9aaa9fe8
ray0324 <ray0324@foxmail.com>
2025-03-14 01:13:21 +0800

    chore: 删除 Button 组件及相关样式，更新富文本编辑器内容

--- packages/editor-kit/src/editor/richtext-editor.tsx
+++ packages/editor-kit/src/editor/richtext-editor.tsx
@@ -198,7 +198,7 @@ const extensions = [
 
 const content = `
 <h2>
-  Hi there,
+  Hi there
 </h2>
 <p>
   this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
--- packages/editor-kit/src/index.tsx
+++ packages/editor-kit/src/index.tsx
@@ -1,7 +1,6 @@
 import RichTextEditor from './editor/richtext-editor';
-import { Button } from './Button';
+// import { Button } from './Button';
 
-console.log('RichTextEditor111', RichTextEditor);
 
 export default RichTextEditor;
-export {  Button };
+// export {  Button };
